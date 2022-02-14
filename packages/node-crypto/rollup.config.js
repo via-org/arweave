f00f298ef5b63d5130fd62d060a97eb876270e1a
Donald Geddes <hi@donaldgeddes.ca>
2022-02-13 23:13:45 -0800

    [dev:node-crypto] Specify external deps

--- packages/node-crypto/rollup.config.js
+++ packages/node-crypto/rollup.config.js
@@ -5,6 +5,7 @@ const input = './src/index.js'
 // eslint-disable-next-line import/no-default-export
 export default {
   input,
+  external: ['crypto', '@panva/asn1.js', '@via-org/data-utils'],
   output: [
     { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'esm' },
