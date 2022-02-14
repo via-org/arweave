265a21e5a33e7a6a20eaebc27110fce5d230930c
Donald Geddes <hi@donaldgeddes.ca>
2022-02-13 22:17:15 -0800

    [dev:node-crypto] Add "sign" method

--- packages/node-crypto/src/index.js
+++ packages/node-crypto/src/index.js
@@ -1,6 +1,8 @@
 import crypto from 'crypto'
 import { concatUint8Arrays } from '@via-org/data-utils'
 
+import { jwkToPem } from './utils/index.js'
+
 export class NodeCrypto {
   constructor() {
     /** @type {crypto} */
@@ -11,6 +13,7 @@ export class NodeCrypto {
    * @param {(Uint8Array|Uint8Array[])} data
    * @param {Object} algo
    * @param {string} algo.node
+   * @param {string} algo.browser
    * @returns {Uint8Array}
    */
   hash(data, algo) {
@@ -19,5 +22,16 @@ export class NodeCrypto {
       .update(Array.isArray(data) ? concatUint8Arrays(data) : data)
       .digest()
     return new Uint8Array(digest)
+  }
+
+  /**
+   * @param {Uint8Array} data
+   * @param {Object<string, string>} jwk
+   * @returns {Uint8Array}
+   */
+  sign(data, jwk) {
+    const key = jwkToPem(jwk)
+    const padding = this.crypto.constants.RSA_PKCS1_PSS_PADDING
+    return this.crypto.createSign('sha256').update(data).sign({ key, padding })
   }
 }
