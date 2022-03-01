cee8b63d40d8599321b3da4ef5cbcee22e8b912c
Donald Geddes <hi@donaldgeddes.ca>
2022-03-01 13:54:15 -0800

    [dev:client] Update config files - Set `ava` concurrency to 1 (best for `arlocal`) - Configure rollup builds - Update package.json deps and fields

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -1,12 +1,64 @@
+import resolve from '@rollup/plugin-node-resolve'
 import pkg from './package.json'
 
-const input = './src/index.js'
+const inputs = {
+  main: './src/index.js',
+  utilities: './src/utilities.js',
+}
 
 // eslint-disable-next-line import/no-default-export
-export default {
-  input,
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'esm' },
-  ],
-}
+export default [
+  /**
+   * Primary export, i.e. @via-org/arweave-client
+   */
+  {
+    /** Browser */
+    input: inputs.main,
+    external: ['cross-fetch'],
+    output: {
+      name: 'Arweave',
+      file: pkg.exports['.'].browser,
+      format: 'iife',
+      globals: {
+        'cross-fetch': 'window.fetch',
+      },
+      // NOTE: removing interop for cross-fetch fixes the `TypeError:
+      // 'fetch' called on an object that does not implement interface
+      // Window.` error
+      interop: id => id !== 'cross-fetch',
+    },
+    plugins: [resolve({ preferBuiltins: false, browser: true })],
+  },
+  {
+    /** ESM + CJS */
+    input: inputs.main,
+    external: [...Object.keys(pkg.dependencies)],
+    output: [
+      { file: pkg.exports['.'].import, format: 'esm' },
+      { file: pkg.exports['.'].require, format: 'cjs' },
+    ],
+  },
+
+  /**
+   * Uilities export, i.e. @via-org/arweave-client/utilities
+   */
+  {
+    /** Browser */
+    input: inputs.utilities,
+    output: {
+      name: 'Utilities',
+      file: pkg.exports['./utilities'].browser,
+      format: 'iife',
+    },
+    plugins: [resolve({ preferBuiltins: false, browser: true })],
+  },
+  {
+    /** ESM + CJS */
+    input: inputs.utilities,
+    external: [...Object.keys(pkg.dependencies), 'url'],
+    output: [
+      { file: pkg.exports['./utilities'].import, format: 'esm' },
+      { file: pkg.exports['./utilities'].require, format: 'cjs' },
+    ],
+  },
+]
